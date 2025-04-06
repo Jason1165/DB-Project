b328e71cdbe69f60b90a7af6a5d2564cab8fdc69
Zesan Rahman <ramenzzz64@gmail.com>
2025-04-06 17:39:43 -0400

    Changed coach earnings like player and team

--- INSERT/COMBINED_INSERT.sql
+++ INSERT/COMBINED_INSERT.sql
@@ -1,20 +1,19 @@
 INSERT INTO coach VALUES 
-(1, 'Michael Malone', 52, '2015-06-15', NULL, 4000000), 
-(2, 'Jacque Vaughn', 49, '2022-11-01', NULL, 3500000), 
-(3, 'Brian Keefe', 46, '2024-01-25', NULL, 2800000), 
-(4, 'Steve Kerr', 58, '2014-05-19', NULL, 7500000), 
-(5, 'Will Hardy', 36, '2022-06-29', NULL, 3200000), 
-(6, 'Gregg Popovich', 75, '1996-12-10', NULL, 11000000), 
-(7, 'Mike Brown', 54, '2022-05-09', NULL, 4500000), 
-(8, 'Erik Spoelstra', 53, '2008-04-28', NULL, 8500000), 
-(9, 'J.B. Bickerstaff', 45, '2021-03-10', NULL, 3800000), 
-(10, 'Tom Thibodeau', 67, '2020-07-30', NULL, 6500000),
-(11, 'Phil Jackson', 78, '1989-07-10', '2011-05-08', 12000000),
-(12, 'Pat Riley', 79, '1981-11-19', '2008-04-16', 9500000),
-(13, 'Red Auerbach', 89, '1950-05-15', '1966-04-28', 800000),
-(14, 'Jerry Sloan', 78, '1988-12-09', '2011-02-10', 5200000),
-(15, 'Don Nelson', 84, '1976-10-14', '2010-04-14', 6100000);
-
+(1, 'Michael Malone', 52, '2015-06-15', NULL, 4),
+(2, 'Jacque Vaughn', 49, '2022-11-01', NULL, 3.5),
+(3, 'Brian Keefe', 46, '2024-01-25', NULL, 2.8),
+(4, 'Steve Kerr', 58, '2014-05-19', NULL, 7.5),
+(5, 'Will Hardy', 36, '2022-06-29', NULL, 3.2),
+(6, 'Gregg Popovich', 75, '1996-12-10', NULL, 11),
+(7, 'Mike Brown', 54, '2022-05-09', NULL, 4.5),
+(8, 'Erik Spoelstra', 53, '2008-04-28', NULL, 8.5),
+(9, 'J.B. Bickerstaff', 45, '2021-03-10', NULL, 3.8),
+(10, 'Tom Thibodeau', 67, '2020-07-30', NULL, 6.5),
+(11, 'Phil Jackson', 78, '1989-07-10', '2011-05-08', 12),
+(12, 'Pat Riley', 79, '1981-11-19', '2008-04-16', 9.5),
+(13, 'Red Auerbach', 89, '1950-05-15', '1966-04-28', 0.8),
+(14, 'Jerry Sloan', 78, '1988-12-09', '2011-02-10', 5.2),
+(15, 'Don Nelson', 84, '1976-10-14', '2010-04-14', 6.1);
 
 INSERT INTO donation (donationID, teamID, sponsorID, amount) VALUES
 (1, 3, 7, 5),
@@ -42,26 +41,26 @@ INSERT INTO `match` VALUES
 
 
 INSERT INTO player VALUES
-(1, 1, 'Nikola Jokic', 'C', 15, 83, 29, 47600000),
-(2, 2, 'Mikal Bridges', 'SF', 1, 78, 27, 23300000),
-(3, 3, 'Jordan Poole', 'SG', 13, 76, 24, 27500000),
-(4, 4, 'Stephen Curry', 'PG', 30, 74, 36, 51900000),
-(5, 5, 'Lauri Markkanen', 'PF', 23, 84, 27, 18000000),
-(6, 6, 'Victor Wembanyama', 'PF', 1, 88, 20, 12500000),
-(7, 7, 'DeAaron Fox', 'PG', 5, 75, 26, 32600000),
-(8, 8, 'Jimmy Butler', 'SF', 22, 79, 34, 45200000),
-(9, 9, 'Cade Cunningham', 'PG', 2, 78, 22, 11055360),
-(10, 10, 'Jalen Brunson', 'PG', 11, 74, 27, 26346666),
-(11, 1, 'Christian Braun', 'SG', 0, 79, 23, 2939280),
-(12, 2, 'Michael Jordan', 'SG', 23, 78, 60, 33140000),
-(13, 6, 'Kobe Bryant', 'SG', 24, 78, 41, 25000000),
-(14, 8, 'Dwyane Wade', 'SG', 3, 76, 42, 20000000),
-(15, 10, 'Patrick Ewing', 'C', 33, 84, 62, 18724000),
-(16, 2, 'Magic Johnson', 'PG', 32, 81, 64, 14660000),
-(17, 4, 'Wilt Chamberlain', 'C', 13, 85, 63, 10000000),
-(18, 3, 'Larry Bird', 'SF', 33, 81, 67, 7070000),
-(19, 5, 'John Stockton', 'PG', 12, 73, 62, 6000000),
-(20, 9, 'Bill Russell', 'C', 6, 82, 89, 100000);
+(1, 1, 'Nikola Jokic', 'C', 15, 83, 29, 47.6),
+(2, 2, 'Mikal Bridges', 'SF', 1, 78, 27, 23.3),
+(3, 3, 'Jordan Poole', 'SG', 13, 76, 24, 27.5),
+(4, 4, 'Stephen Curry', 'PG', 30, 74, 36, 51.9),
+(5, 5, 'Lauri Markkanen', 'PF', 23, 84, 27, 18.0),
+(6, 6, 'Victor Wembanyama', 'PF', 1, 88, 20, 12.5),
+(7, 7, 'DeAaron Fox', 'PG', 5, 75, 26, 32.6),
+(8, 8, 'Jimmy Butler', 'SF', 22, 79, 34, 45.2),
+(9, 9, 'Cade Cunningham', 'PG', 2, 78, 22, 11.06),
+(10, 10, 'Jalen Brunson', 'PG', 11, 74, 27, 26.35),
+(11, 1, 'Christian Braun', 'SG', 0, 79, 23, 2.94),
+(12, 2, 'Michael Jordan', 'SG', 23, 78, 60, 33.14),
+(13, 6, 'Kobe Bryant', 'SG', 24, 78, 41, 25.0),
+(14, 8, 'Dwyane Wade', 'SG', 3, 76, 42, 20.0),
+(15, 10, 'Patrick Ewing', 'C', 33, 84, 62, 18.72),
+(16, 2, 'Magic Johnson', 'PG', 32, 81, 64, 14.66),
+(17, 4, 'Wilt Chamberlain', 'C', 13, 85, 63, 10.0),
+(18, 3, 'Larry Bird', 'SF', 33, 81, 67, 7.07),
+(19, 5, 'John Stockton', 'PG', 12, 73, 62, 6.0),
+(20, 9, 'Bill Russell', 'C', 6, 82, 89, 0.1);
 
 
 INSERT INTO playoff VALUES
@@ -91,16 +90,16 @@ INSERT INTO referee VALUES
 
 
 INSERT INTO team VALUES
-(1, 1, 1, 3, 2, 'Denver Nuggets', 1, 3, 350000000),
-(2, 2, 2, 1, 1, 'Brooklyn Nets', 0, 1, 310000000),
-(3, 3, 3, 5, 1, 'Washington Wizards', 1, 4, 320000000),
-(4, 4, 4, 9, 2, 'Golden State Warriors', 7, 12, 510000000),
-(5, 5, 5, 8, 2, 'Utah Jazz', 0, 2, 340000000),
-(6, 6, 6, 4, 2, 'San Antonio Spurs', 5, 10, 390000000),
-(7, 7, 7, 10, 2, 'Sacramento Kings', 0, 1, 300000000),
-(8, 8, 8, 2, 1, 'Miami Heat', 3, 9, 380000000),
-(9, 9, 9, 6, 1, 'Detroit Pistons', 3, 5, 290000000),
-(10, 10, 10, 7, 1, 'New York Knicks', 2, 8, 425000000);
+(1, 1, 1, 3, 2, 'Denver Nuggets', 1, 3, 350),
+(2, 2, 2, 1, 1, 'Brooklyn Nets', 0, 1, 310),
+(3, 3, 3, 5, 1, 'Washington Wizards', 1, 4, 320),
+(4, 4, 4, 9, 2, 'Golden State Warriors', 7, 12, 510),
+(5, 5, 5, 8, 2, 'Utah Jazz', 0, 2, 340),
+(6, 6, 6, 4, 2, 'San Antonio Spurs', 5, 10, 390),
+(7, 7, 7, 10, 2, 'Sacramento Kings', 0, 1, 300),
+(8, 8, 8, 2, 1, 'Miami Heat', 3, 9, 380),
+(9, 9, 9, 6, 1, 'Detroit Pistons', 3, 5, 290),
+(10, 10, 10, 7, 1, 'New York Knicks', 2, 8, 425);
 
 
 INSERT INTO bracket VALUES
