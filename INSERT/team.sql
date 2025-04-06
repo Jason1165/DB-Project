e4299c520e5457fa889a056cc710117d8f5a1568
Zesan Rahman <ramenzzz64@gmail.com>
2025-04-06 17:29:20 -0400

    Changed the earnings so they are floats without so many 0s

--- INSERT/team.sql
+++ INSERT/team.sql
@@ -1,11 +1,12 @@
 INSERT INTO team VALUES
-(1, 1, 1, 3, 2, 'Denver Nuggets', 1, 3, 350000000),
-(2, 2, 2, 1, 1, 'Brooklyn Nets', 0, 1, 310000000),
-(3, 3, 3, 5, 1, 'Washington Wizards', 1, 4, 320000000),
-(4, 4, 4, 9, 2, 'Golden State Warriors', 7, 12, 510000000),
-(5, 5, 5, 8, 2, 'Utah Jazz', 0, 2, 340000000),
-(6, 6, 6, 4, 2, 'San Antonio Spurs', 5, 10, 390000000),
-(7, 7, 7, 10, 2, 'Sacramento Kings', 0, 1, 300000000),
-(8, 8, 8, 2, 1, 'Miami Heat', 3, 9, 380000000),
-(9, 9, 9, 6, 1, 'Detroit Pistons', 3, 5, 290000000),
-(10, 10, 10, 7, 1, 'New York Knicks', 2, 8, 425000000);
+(1, 1, 1, 3, 2, 'Denver Nuggets', 1, 3, 350),
+(2, 2, 2, 1, 1, 'Brooklyn Nets', 0, 1, 310),
+(3, 3, 3, 5, 1, 'Washington Wizards', 1, 4, 320),
+(4, 4, 4, 9, 2, 'Golden State Warriors', 7, 12, 510),
+(5, 5, 5, 8, 2, 'Utah Jazz', 0, 2, 340),
+(6, 6, 6, 4, 2, 'San Antonio Spurs', 5, 10, 390),
+(7, 7, 7, 10, 2, 'Sacramento Kings', 0, 1, 300),
+(8, 8, 8, 2, 1, 'Miami Heat', 3, 9, 380),
+(9, 9, 9, 6, 1, 'Detroit Pistons', 3, 5, 290),
+(10, 10, 10, 7, 1, 'New York Knicks', 2, 8, 425);
+
