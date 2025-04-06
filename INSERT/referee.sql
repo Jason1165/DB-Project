6e7d9bb88a4eb77b48929c34bfe09f190ede59be
mofeimw <mofei.mw@gmail.com>
2025-04-06 00:11:20 -0400

    coach, player, referee, team insertion

---
+++ INSERT/referee.sql
@@ -0,0 +1,11 @@
+INSERT INTO referee VALUES
+(1, 4, 'Scott Foster', 57, '1994-09-30', NULL),
+(2, 8, 'Tony Brothers', 59, '1994-10-15', NULL),
+(3, 1, 'Marc Davis', 55, '1998-12-10', NULL),
+(4, 10, 'James Capers', 61, '1995-01-25', NULL),
+(5, 3, 'Zach Zarba', 49, '2003-04-12', NULL),
+(6, 7, 'Ed Malloy', 53, '2002-09-05', NULL),
+(7, 5, 'John Goble', 47, '2007-11-30', NULL),
+(8, 2, 'David Guthrie', 48, '2005-03-18', NULL),
+(9, 6, 'Joey Crawford', 72, '1977-05-12', '2016-04-10'),
+(10, 9, 'Dick Bavetta', 84, '1975-12-02', '2014-08-20');
