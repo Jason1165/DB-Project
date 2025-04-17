fca6f0b679f3a5f799c23aaae8bcf4ccb935c01f
Zesan Rahman <ramenzzz64@gmail.com>
2025-04-17 17:30:15 -0400

    Streaming service inputs adjusted for new rating system

--- INSERT/COMBINED_INSERT.sql
+++ INSERT/COMBINED_INSERT.sql
@@ -58,16 +58,16 @@ INSERT INTO conference VALUES
 
 
 INSERT INTO streaming_service VALUES
-(1, 'DIRECTV Stream', 79.99, 4),
-(2, 'Hulu', 7.99, 4),
-(3, 'NBA TV', 6.99, 4),
-(4, 'Sling TV', 61.00, 3),
-(5, 'YouTube TV', 82.99, 4),
-(6, 'ESPN+', 10.99, 4),
-(7, 'Fubo', 74.99, 4),
-(8, 'NBA League Pass', 14.99, 4),
-(9, 'Max', 16.99, 4),
-(10, 'Amazon Prime', 14.99, 4);
+(1, 'DIRECTV Stream', 79.99, 0, 0),
+(2, 'Hulu', 7.99, 0, 0),
+(3, 'NBA TV', 6.99, 0, 0),
+(4, 'Sling TV', 61.00, 0, 0),
+(5, 'YouTube TV', 82.99, 0, 0),
+(6, 'ESPN+', 10.99, 0, 0),
+(7, 'Fubo', 74.99, 0, 0),
+(8, 'NBA League Pass', 14.99, 0, 0),
+(9, 'Max', 16.99, 0, 0),
+(10, 'Amazon Prime', 14.99, 0, 0);
 
 
 INSERT INTO bracket VALUES
