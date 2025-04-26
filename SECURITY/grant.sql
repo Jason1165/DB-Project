84046b8ddb23d3c93765b3a854a7f0045cbd93e8
Zesan <ramenzzz64@gmail.com>
2025-04-26 14:30:56 -0400

    Don't need flush privilege

--- SECURITY/grant.sql
+++ SECURITY/grant.sql
@@ -5,7 +5,6 @@ GRANT ALL PRIVILEGES ON *.* TO 'Sadmin';
 -- Create user role
 CREATE ROLE 'Suser';
 
--- Replace 'your_database' with your actual database name
 GRANT SELECT ON db4.player TO 'Suser';
 GRANT SELECT ON db4.team TO 'Suser';
 GRANT SELECT ON db4.match TO 'Suser';
@@ -15,5 +14,3 @@ GRANT SELECT ON db4.conference TO 'Suser';
 GRANT SELECT ON db4.rating TO 'Suser';
 GRANT INSERT, UPDATE, DELETE ON db4.rating TO 'Suser';
 
--- Make sure to flush privileges
-FLUSH PRIVILEGES;
