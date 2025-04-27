d63a0f9717563aa625342f1d5e189e4c58353b54
Zesan <ramenzzz64@gmail.com>
2025-04-27 15:55:35 -0400

    Testing updated grants file

--- SECURITY/GRANTS.sql
+++ SECURITY/GRANTS.sql
@@ -1,16 +1,29 @@
--- Admin privileges
-CREATE ROLE 'Sadmin';
-GRANT ALL PRIVILEGES ON db4.* TO 'Sadmin';
-
 -- User privileges
+DROP ROLE 'Suser';
 CREATE ROLE 'Suser';
 
-GRANT SELECT ON db4.player TO 'Suser';
-GRANT SELECT ON db4.team TO 'Suser';
-GRANT SELECT ON db4.match TO 'Suser';
-GRANT SELECT ON db4.streaming_service TO 'Suser';
-GRANT SELECT ON db4.bracket TO 'Suser';
-GRANT SELECT ON db4.conference TO 'Suser';
-GRANT SELECT ON db4.rating TO 'Suser';
+-- Views for public
+CREATE OR REPLACE VIEW playerPublic AS 
+SELECT name, position, number, height, age, salary
+FROM db4.player;
+
+CREATE OR REPLACE VIEW teamPublic AS
+SELECT name, championships_won, playoffs_won, earnings
+FROM db4.team;
+
+CREATE OR REPLACE VIEW ratingPublic AS
+SELECT score
+FROM db4.rating;
+-- Granting said views
+GRANT SELECT ON db4.playerPublic TO 'Suser';
+GRANT SELECT ON db4.teamPublic TO 'Suser';
+GRANT SELECT ON db4.ratingPublic TO 'Suser';
+-- I don't believe we need these as of now because these aren't really searchable
+-- uncomment if it breaks things in accessing said info, then comment the views out 
+-- GRANT SELECT ON db4.match TO 'Suser';
+-- GRANT SELECT ON db4.streaming_service TO 'Suser';
+-- GRANT SELECT ON db4.bracket TO 'Suser';
+-- GRANT SELECT ON db4.conference TO 'Suser';
+-- GRANT SELECT ON db4.rating TO 'Suser';
 
 GRANT INSERT, UPDATE, DELETE ON db4.rating TO 'Suser';
