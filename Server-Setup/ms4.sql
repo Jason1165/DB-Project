5c6011810b61b5a4503f124bfc5efe9142737055
Jason Lin <73240290+Jason1165@users.noreply.github.com>
2025-04-27 20:23:44 -0400

    mirror changes in ms4

--- Server-Setup/ms4.sql
+++ Server-Setup/ms4.sql
@@ -780,8 +780,9 @@ DROP PROCEDURE IF EXISTS GetBracketByTeam;
 DELIMITER $$
 CREATE PROCEDURE GetBracketByTeam(IN teamName VARCHAR(255))
 BEGIN
-    SELECT DISTINCT m.bracketID
+    SELECT DISTINCT b.bracketID, b.season
     FROM `match` m
+    JOIN bracket b ON m.bracketID = b.bracketID
     JOIN team t1 ON m.homeTeamID = t1.teamID
     JOIN team t2 ON m.visitingTeamID = t2.teamID
     WHERE t1.name LIKE CONCAT('%', teamName, '%') OR t2.name LIKE CONCAT('%', teamName,'%');
@@ -791,9 +792,9 @@ DELIMITER ;
 
 DROP PROCEDURE IF EXISTS GetBracketBySeason;
 DELIMITER $$
-CREATE PROCEDURE GetBracketBySeason(IN season VARCHAR(255))
+CREATE OR REPLACE PROCEDURE GetBracketBySeason(IN season VARCHAR(255))
 BEGIN
-    SELECT DISTINCT b.bracketID
+    SELECT DISTINCT b.bracketID, b.season
     FROM bracket b
     WHERE b.season LIKE CONCAT('%', season, '%');
 END$$
